1835aab9df975eb629b80106f135dbcf90189e85
Tafoya <Tafoya.Johnathan@epa.gov>
2020-11-17 08:50:32 -0500

    Updated more components

--- app/src/components/SubregionCharts.vue
+++ app/src/components/SubregionCharts.vue
@@ -1,27 +1,74 @@
+<i18n>
+{
+    "en": {
+        "fuelMixHeader": "Fuel Mix",
+        "fuelMixBody": {
+            "text": "This chart compares fuel mix (%) of sources used to generate electricity in the selected {eGRIDSubregion} to the national fuel mix (%).",
+            "eGRIDSubregion": "eGRID subregion"
+        },
+        "emissionRateHeader": "Emission Rates",
+        "emissionRateBody": {
+            "text": "This chart compares the average emission rates in pounds per {MWhInfo} in the selected {eGRIDSubregion} to the national average emission rates for {co2}, {so2}, and {nox}.",
+            "MWhInfo": "MWh",
+            "eGRIDSubregion": "eGRID subregion",
+            "co2": "carbon dioxide",
+            "so2": "sulfur dioxide",
+            "nox": "nitrogen oxide",
+            "select": "Select:"
+        },
+        "printReport": "Print Report"
+    },
+    "es": {
+        "fuelMixHeader": "Combinación de combustibles",
+        "fuelMixBody": {
+            "text": "Este gráfico compara la combinación de combustibles (porcentajes) de las fuentes utilizadas para generar electricidad en la {eGRIDSubregion} seleccionada con la combinación de combustibles (porcentajes) nacional.",
+            "eGRIDSubregion": "subregiones de eGRID"
+        },
+        "emissionRateHeader": "Tasas de emisión",
+        "emissionRateBody": {
+            "text": "Este cuadro compara las tasas de emisión promedio en libras por {MWhInfo} en las {eGRIDSubregion} con las tasas de emisión nacionales promedio para {co2}, {so2}, y {nox}.",
+            "MWhInfo": "MWh",
+            "eGRIDSubregion": "subregion de eGRID",
+            "co2": "dióxido de carbono",
+            "so2": "dióxido de azufre",
+            "nox": "óxido de nitrógeno",
+            "select": "Seleccione:"
+        },
+        "printReport": "Print Report"
+    }
+}
+</i18n>
 <template>
   <div v-if="dataLoaded">
     <div class="col row cols-3" id="kpis">
       <h3 id="kpis-section-title">
-        <span>{{ subregion.properties.name }}</span> Emission Rates
+        <span>{{ subregion.properties.name }}</span>
+        {{ $t("emissionRateHeader") }}
       </h3>
       <div class="col box special" value="co2EmissionRate">
         <h3 class="pane-title">CO<sub>2</sub></h3>
         <div class="pane-content">
-          <h2 id="co2-pane-content">{{ subregion.properties.emissionFactor.co2EmissionRate.display }}</h2>
+          <h2 id="co2-pane-content">
+            {{ subregion.properties.emissionFactor.co2EmissionRate.display }}
+          </h2>
           <p>(lbs/MWh)</p>
         </div>
       </div>
       <div class="col box special" value="so2EmissionRate">
         <h3 class="pane-title">SO<sub>2</sub></h3>
         <div class="pane-content">
-          <h2 id="so2-pane-content">{{ subregion.properties.emissionFactor.so2EmissionRate.display }}</h2>
+          <h2 id="so2-pane-content">
+            {{ subregion.properties.emissionFactor.so2EmissionRate.display }}
+          </h2>
           <p>(lbs/MWh)</p>
         </div>
       </div>
       <div class="col box special" value="noxEmissionRate">
         <h3 class="pane-title">NO<sub>X</sub></h3>
         <div class="pane-content">
-          <h2 id="nox-pane-content">{{ subregion.properties.emissionFactor.noxEmissionRate.display }}</h2>
+          <h2 id="nox-pane-content">
+            {{ subregion.properties.emissionFactor.noxEmissionRate.display }}
+          </h2>
           <p>(lbs/MWh)</p>
         </div>
       </div>
@@ -31,18 +78,61 @@
         <router-link :to="'/'">« Back to All Subregions</router-link>
       </p>
       <div id="fuelMixContainer" class="col">
-          <h3>Fuel Mix</h3>
-          <p>This chart compares fuel mix (%) of sources used to generate electricity in the selected <a href="https://www.epa.gov/sites/production/files/styles/large/public/2020-03/2018_egrid_subregions.png" target="_blank">eGRID subregion</a> to the national fuel mix (%).</p>
-          <subregionFuelMixChart></subregionFuelMixChart>
+        <h3>{{ $t("fuelMixHeader") }}</h3>
+        <i18n path="fuelMixBody.text" tag="p">
+          <template #eGRIDSubregion>
+            <a
+              href="https://www.epa.gov/sites/production/files/styles/large/public/2020-03/2018_egrid_subregions.png"
+              target="_blank"
+              >{{ $t("fuelMixBody.eGRIDSubregion") }}</a
+            >
+          </template>
+        </i18n>
+        <subregionFuelMixChart></subregionFuelMixChart>
       </div>
       <div id="printMap"></div>
       <div id="emissionRateContainer" class="col">
-          <h3>Emission Rates</h3>
-          <p id="emRatesDescription">This chart compares the average emission rates (lbs/MWh) in the selected <a href="https://www.epa.gov/sites/production/files/styles/large/public/2020-03/2018_egrid_subregions.png" target="_blank">eGRID subregion</a> to the national average emission rates (lbs/MWh) for <a href="/ghgemissions/overview-greenhouse-gases#carbon-dioxide" target="_blank">carbon dioxide (CO<sub>2</sub>)</a>, <a href="/so2-pollution" target="_blank">sulfur dioxide (SO<sub>2</sub>)</a>, and <a href="/no2-pollution" target="_blank">nitrogen oxide (NO<sub>X</sub>)</a>.</p>
-          <subregionEmissionRateChart></subregionEmissionRateChart>
+        <h3>{{ $t("emissionRateHeader") }}</h3>
+        <i18n path="emissionRateBody.text" tag="p">
+          <template #MWhInfo>
+            <a
+              @click="$parent.$parent.showMegaWattInfo = true"
+              href="javascript:void(0)"
+              >{{ $t("emissionRateBody.MWhInfo") }}</a
+            >
+          </template>
+          <template #eGRIDSubregion>
+            <a
+              href="https://epa.gov/sites/production/files/styles/large/public/2020-03/2018_egrid_subregions.png"
+              target="_blank"
+              >{{ $t("emissionRateBody.eGRIDSubregion") }}</a
+            >
+          </template>
+          <template #co2>
+            <a
+              href="/ghgemissions/overview-greenhouse-gases#carbon-dioxide"
+              target="_blank"
+              >{{ $t("emissionRateBody.co2") }} (CO<sub>2</sub>)</a
+            >
+          </template>
+          <template #so2>
+            <a href="/so2-pollution" target="_blank"
+              >{{ $t("emissionRateBody.so2") }} (SO<sub>2</sub>)</a
+            >
+          </template>
+          <template #nox>
+            <a href="/no2-pollution" target="_blank"
+              >{{ $t("emissionRateBody.nox") }} (NO<sub>X</sub>)</a
+            >
+          </template>
+        </i18n>
+
+        <subregionEmissionRateChart></subregionEmissionRateChart>
       </div>
     </div>
-    <p><a href="javascript:window.print()">&#128438; Print Report</a></p>
+    <p>
+      <a href="javascript:window.print()">&#128438; {{ $t("printReport") }}</a>
+    </p>
     <emissionsCalculator></emissionsCalculator>
   </div>
 </template>
@@ -55,48 +145,48 @@ import subregionEmissionRateChart from "./SubregionEmissionRateChart.vue";
 import emissionsCalculator from "./EmissionsCalculator.vue";
 
 export default {
-    components: {
-        subregionFuelMixChart: subregionFuelMixChart,
-        subregionEmissionRateChart: subregionEmissionRateChart,
-        emissionsCalculator: emissionsCalculator,
-    },
-    data() {
-        return {
-            subregion: {},
-            dataLoaded: false
-        };
-    },
-    mounted: function() {
+  components: {
+    subregionFuelMixChart: subregionFuelMixChart,
+    subregionEmissionRateChart: subregionEmissionRateChart,
+    emissionsCalculator: emissionsCalculator,
+  },
+  data() {
+    return {
+      subregion: {},
+      dataLoaded: false,
+    };
+  },
+  mounted: function () {
+    this.subregion = selectedSubregion.data;
+    this.dataLoaded = true;
+    this.$root.$children[0].subregionSelected = true;
+    this.$root.$children[0].showMain = false;
+  },
+  watch: {
+    $route: function (to, from) {
+      var self = this;
+      if (to !== from) {
         this.subregion = selectedSubregion.data;
-        this.dataLoaded = true;
-        this.$root.$children[0].subregionSelected = true
-        this.$root.$children[0].showMain = false
+        this.$children[1].update();
+        this.$children[2].update();
+      }
     },
-    watch: {
-        $route: function(to, from) {
-          var self = this
-          if (to !== from) {
-              this.subregion = selectedSubregion.data
-              this.$children[1].update()
-              this.$children[2].update()
-          }
-        }
-    },
-    beforeCreate: function() {
-        var self = this;
-        var subName = this.$router.history.current.params.subregion;
-        var subregion = allSubregions.data.filter(function(d) {
-            return d.properties.name == subName;
-        });
-        if (subregion.length > 0) {
-            $(".map").css({"fill":""});
-            $("." + subregion[0].properties.name).css({"fill":"steelblue"});
-            $("#regionSelectionDropdown").val(subregion[0].properties.name);
-            selectedSubregion.update(subregion[0]);
-        } else {
-            this.dataLoaded = false;
-            self.$router.push("/");
-        }
+  },
+  beforeCreate: function () {
+    var self = this;
+    var subName = this.$router.history.current.params.subregion;
+    var subregion = allSubregions.data.filter(function (d) {
+      return d.properties.name == subName;
+    });
+    if (subregion.length > 0) {
+      $(".map").css({ fill: "" });
+      $("." + subregion[0].properties.name).css({ fill: "steelblue" });
+      $("#regionSelectionDropdown").val(subregion[0].properties.name);
+      selectedSubregion.update(subregion[0]);
+    } else {
+      this.dataLoaded = false;
+      self.$router.push("/");
     }
+  },
 };
 </script>
--- app/src/components/SubregionEmissionRateChart.vue
+++ app/src/components/SubregionEmissionRateChart.vue
@@ -1,11 +1,24 @@
+<i18n>
+{
+  "en": {
+    "select": "Select"
+  },
+  "es": {
+    "select": "Seleccione"
+  }
+}
+</i18n>
+
 <template>
   <div id="emissionRate">
     <div id="select-offset-2">
       <p class="select-pollutant-label">
-        <strong>Select:</strong>
+        <strong>{{ $t("select") }}</strong>
       </p>
       <div id="pollutantSelectSub">
-        <button id="defaultPollutantSub" value="co2EmissionRate">CO<sub>2</sub></button>
+        <button id="defaultPollutantSub" value="co2EmissionRate">
+          CO<sub>2</sub>
+        </button>
         <button value="so2EmissionRate">SO<sub>2</sub></button>
         <button value="noxEmissionRate">NO<sub>X</sub></button>
       </div>
@@ -16,57 +29,58 @@
 import {
   addLogoBottom,
   formatFuelLabel,
-  checkNational
+  checkNational,
 } from "../helpers/ChartHelpers.js";
 import { nationalFeature } from "../stores/nationalFeature.js";
 import { selectedSubregion } from "../stores/selectedSubregion.js";
-import { addTooltip } from '../helpers/Tooltip';
+import { addTooltip } from "../helpers/Tooltip";
 
 export default {
   data() {
     return {
       data: [],
-      selectedPollutantSub: "co2EmissionRate"
+      selectedPollutantSub: "co2EmissionRate",
     };
   },
-  mounted: function() {
-    var self = this
+  mounted: function () {
+    var self = this;
     this.data = [nationalFeature.data[0], selectedSubregion.data];
     this.display(this.data);
 
-    $(window).on('resize', function(){
+    $(window).on("resize", function () {
       d3.selectAll("#emissionRate svg").remove();
-      self.update()
-    })
-
-    $("#pollutantSelectSub button").on("click", function(e){
-        $(this).css({"background-color": "#2B7D3B"});
-        $(this).siblings().css({"background-color":"#0071bc"});
-        self.selectedPollutantSub = $(this).val();
-
-        $("#sub" + self.selectedPollutantSub).show();
-        $("#sub" + self.selectedPollutantSub).siblings("svg").hide();
+      self.update();
     });
 
-    $("#defaultPollutantSub").trigger("click")
+    $("#pollutantSelectSub button").on("click", function (e) {
+      $(this).css({ "background-color": "#2B7D3B" });
+      $(this).siblings().css({ "background-color": "#0071bc" });
+      self.selectedPollutantSub = $(this).val();
 
+      $("#sub" + self.selectedPollutantSub).show();
+      $("#sub" + self.selectedPollutantSub)
+        .siblings("svg")
+        .hide();
+    });
+
+    $("#defaultPollutantSub").trigger("click");
   },
   methods: {
-    display: function(data) {
+    display: function (data) {
       var domElement = "#emissionRate";
       var w = $(domElement).width();
       var h = 500 - ($("#select-offset-2").height() - 20);
       var parameters = [
         "so2EmissionRate",
         "co2EmissionRate",
-        "noxEmissionRate"
+        "noxEmissionRate",
       ];
       var emRatesColors = {
         national: "#2b83ba",
         subregion: "#e66101",
         co2EmissionRate: "#d7191c",
         so2EmissionRate: "#008837",
-        noxEmissionRate: "#7b3294"
+        noxEmissionRate: "#7b3294",
       };
 
       for (var i = 0; i < parameters.length; i += 1) {
@@ -85,17 +99,9 @@ export default {
           .range([emRatesColors.national, emRatesColors[selectedPollutantSub]])
           .domain([data[0].properties.name, data[1].properties.name]);
 
-        var xAxis = d3.svg
-          .axis()
-          .outerTickSize(0)
-          .scale(x)
-          .orient("bottom");
+        var xAxis = d3.svg.axis().outerTickSize(0).scale(x).orient("bottom");
 
-        var yAxis = d3.svg
-          .axis()
-          .outerTickSize(0)
-          .scale(y)
-          .orient("left");
+        var yAxis = d3.svg.axis().outerTickSize(0).scale(y).orient("left");
 
         var svg = d3
           .select(domElement)
@@ -110,48 +116,48 @@ export default {
           );
 
         x.domain(
-          data.map(function(d) {
+          data.map(function (d) {
             return d.properties.name;
           })
         );
         y.domain([
           0,
-          d3.max(data, function(d) {
+          d3.max(data, function (d) {
             return d.properties.emissionFactor[selectedPollutantSub].value;
-          })
+          }),
         ]);
 
-        svg.append("g")
+        svg
+          .append("g")
           .attr("class", "x axis")
           .attr("transform", "translate(0," + height + ")")
           .call(xAxis);
 
-        svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis);
+        svg.append("g").attr("class", "y axis").call(yAxis);
 
-        var pollutantLabelTooltip = ""
-        svg.append("text")
+        var pollutantLabelTooltip = "";
+        svg
+          .append("text")
           .attr("y", -30)
           .attr("dy", "0.71em")
           .attr("text-anchor", "end")
           .append("tspan")
-          .text(function(d) {
+          .text(function (d) {
             if (selectedPollutantSub == "co2EmissionRate") {
-              pollutantLabelTooltip = "carbon dioxide"
+              pollutantLabelTooltip = "carbon dioxide";
               return "CO";
             } else if (selectedPollutantSub == "so2EmissionRate") {
-              pollutantLabelTooltip = "sulfur dioxide"
+              pollutantLabelTooltip = "sulfur dioxide";
               return "SO";
             } else if (selectedPollutantSub == "noxEmissionRate") {
-              pollutantLabelTooltip = "nitrogen oxides"
+              pollutantLabelTooltip = "nitrogen oxides";
               return "NO";
             }
           })
           .append("tspan")
-          .attr("baseline-shift","-.25em")
-          .attr("font-size","76.4706%")
-          .text(function(d) {
+          .attr("baseline-shift", "-.25em")
+          .attr("font-size", "76.4706%")
+          .text(function (d) {
             if (selectedPollutantSub == "co2EmissionRate") {
               return "2";
             } else if (selectedPollutantSub == "so2EmissionRate") {
@@ -161,72 +167,83 @@ export default {
             }
           });
 
-        svg.append("text")
+        svg
+          .append("text")
           .attr("y", -50)
           .attr("x", -(height / 2))
           .attr("transform", "rotate(-90)")
           .text("(lbs/MWh)");
 
-        var container = svg.selectAll(".container")
+        var container = svg
+          .selectAll(".container")
           .data(data)
-          .enter().append("g")
-          .attr("class","subEmissionRateTooltip")
-          .attr("title", function(d){
-            return "Average " + pollutantLabelTooltip + " rate: " + d.properties.emissionFactor[parameters[i]].display + " lbs/MWh"
-          })
+          .enter()
+          .append("g")
+          .attr("class", "subEmissionRateTooltip")
+          .attr("title", function (d) {
+            return (
+              "Average " +
+              pollutantLabelTooltip +
+              " rate: " +
+              d.properties.emissionFactor[parameters[i]].display +
+              " lbs/MWh"
+            );
+          });
 
-        container.append("rect")
+        container
+          .append("rect")
           .attr("class", "bar subregionEmissionRate " + parameters[i])
-          .attr("fill", function(d) {
+          .attr("fill", function (d) {
             return color(d.properties.name);
           })
-          .attr("x", function(d) {
+          .attr("x", function (d) {
             return x(d.properties.name);
           })
-          .attr("y", function(d) {
+          .attr("y", function (d) {
             return y(d.properties.emissionFactor[selectedPollutantSub].value);
           })
           .attr("width", x.rangeBand())
-          .attr("height", function(d) {
+          .attr("height", function (d) {
             return (
               height -
               y(d.properties.emissionFactor[selectedPollutantSub].value)
             );
           });
 
-        svg.selectAll("text.bar")
+        svg
+          .selectAll("text.bar")
           .data(data)
           .enter()
           .append("text")
           .attr("text-anchor", "middle")
-          .attr("x", function(d) {
+          .attr("x", function (d) {
             return x(d.properties.name) + x.rangeBand() / 2;
           })
-          .attr("y", function(d) {
+          .attr("y", function (d) {
             return (
               y(d.properties.emissionFactor[selectedPollutantSub].value) + -10
             );
           })
-          .text(function(d) {
+          .text(function (d) {
             return d.properties.emissionFactor[selectedPollutantSub].display;
           });
 
         var baseline = (height + margin.top + margin.bottom) * 0.17;
         addLogoBottom(svg, width - 100, height + baseline);
       }
-      addTooltip(".subEmissionRateTooltip")
-
+      addTooltip(".subEmissionRateTooltip");
     },
-    update: function() {
-      var self = this
+    update: function () {
+      var self = this;
       this.data = [nationalFeature.data[0], selectedSubregion.data];
       d3.selectAll("#emissionRate svg").remove();
       this.display(this.data);
-      $("#pollutantSelectSub button").each(function(){
-        if($(this)[0].value == self.selectedPollutantSub) $(this).trigger("click")
-      })
-    }
-  }
+      $("#pollutantSelectSub button").each(function () {
+        if ($(this)[0].value == self.selectedPollutantSub)
+          $(this).trigger("click");
+      });
+    },
+  },
 };
 </script>
 
