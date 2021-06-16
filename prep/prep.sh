84fec3f9d36d0dbb079ef0a27dc447ccff42b993
Mike Heese <heese.michael@epa.gov>
2021-06-16 11:18:58 -0400

    Makes prepping data a little easier

---
+++ prep/prep.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+echo "Adding fuel breakdown"
+python add_fuel_breakdown.py
+
+echo "Creating the subregion geojson file"
+cd data/shape
+mapshaper eGRID2019_subregions.shp -simplify 1% -o format=geojson
+cd -
+
+echo "Adding the states" 
+node add_states.js
+
+echo "Adding the eGRID data"
+python add_egrid_data.py
+
+echo "Simplify the subregion.json file"
+cd result 
+mapshaper subregion.json -simplify dp 5% -o force format=geojson
+cd -
+
+echo "Copy the result to the app/data directory"
+cp result/subregion.json ../app/data
