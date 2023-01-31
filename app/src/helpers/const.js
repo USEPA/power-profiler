dc6a15176cdec00b7266fbb098cf0392c8c36f97
Johnathan Tafoya <65174118+j-tafoya@users.noreply.github.com>
2023-01-31 11:42:17 -0500

    Update const.js to add comments

--- app/src/helpers/const.js
+++ app/src/helpers/const.js
@@ -1,12 +1,15 @@
 export const constVal = {
-           // CO2 sequestered annually per urban tree planted and allowed to grow for 10years
+           // CO2 lbs sequestered annually per urban tree planted and allowed to grow for 10 years
            carbonSequesteredByTree: 0.06,
-           // CO2 sequestered annually by one acre of average U.S. forest
+           // CO2 lbs sequestered annually by one acre of average U.S. forest
            carbonSequesteredByYear: 0.82,
-           // Conversion Factor for carbon sequestered by one acre of forest preserved from conversion to cropland (in the year of conversion)
+           // Conversion Factor for carbon sequestered by one acre of forest preserved from conversion to cropland  in the year of conversion)
            carbonSequesteredByAcre: 146.27,
+           // Average energy consumption kwh per square foot  per month for commercial calculations
            avgConsumptionSqft: 1.22,
+           // National total energy consumption for one year kwh (nationalAverage * 12)
            nationalTotal: 12188,
+           // Average national energy consumption kwh for one month
            nationalAverage: 1015.667,
            egridSubregionMapUrl:
                "https://www.epa.gov/system/files/images/2022-01/egrid2020_subregion_map.png",
